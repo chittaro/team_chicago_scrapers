6b08286719da62ae944dc584c8d014bb6116d3e7
Bella <chittaro@umich.edu>
2025-04-28 19:02:52 +0200

    add simple API endpoints for URL fetching

--- interface/backend/app.py
+++ interface/backend/app.py
@@ -1,13 +1,45 @@
-# backend/app.py
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, flask
 from flask_cors import CORS
+
+import sys, os
+# make higher-level functions accessible
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from url_generator.search_url_gen import get_company_partnership_urls
+from name_finder.name_type_finder import get_html, get_names, get_all_names, clear_html
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
-def get_competitors():
+def home_route():
     return jsonify({"status": "yass"})
+
+
+@app.route('/api/get_urls/<company>/', methods=['GET'])
+def get_competitor_urls(company):
+    urls = get_company_partnership_urls(company)
+    return flask.jsonify({"urls": urls})
+
+'''
+TODO: change URL-->HTML to only read files that are left over from UI (ignore Xed out files)
+'''
+@app.route('/api/process_urls/<company>/', methods=['GET'])
+def fetch_competitor_urls(company):
+    clear_html(company)
+    get_html(company)
+    return flask.jsonify({"success": True})
+
+@app.route('/api/get_partner_data/<company>/', methods=['GET'])
+def get_partner_data(company):
+    data = {
+        "row": {
+            "partner_name": "PARTNER A",
+            "url_sources": ["www.test.com", "www.test2.com"]
+        }
+    }
+
+    return flask.jsonify(**data), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
